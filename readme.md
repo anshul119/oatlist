d7acc4daa3c29674d6d4138c22cd4413aa83b99c
anshul119 <assharmaanshul119@gmail.com>
2018-11-10 17:26:22 +0100

    tsconfig and linting

---
+++ readme.md
@@ -0,0 +1,13 @@
+# [OatList]
+
+Oatlist fetches the list of users form OAT list users API. It has been made with React with TypeScript.
+
+By using interfaces and models the application is strongly typed making it very easy to catch the errors, beauty of TypeScript. I used Styled Components to style the components.
+
+Webpack takes care of all the bundling and other necessary build tasks.
+
+## Getting Started
+
+For running development build on localhost please install node modules by runnins `npm i` and then run the webpack dev server `npm run serve`.
+
+Head to `localhost:8000` to see the app.
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = environment => {
+	const isProd = 'production'.indexOf(environment) !== -1;
+	return {
+		entry: path.join(__dirname, 'src', 'index.tsx'),
+		output: {
+			filename: 'bundle.js',
+			path: path.join(__dirname, 'dist')
+		},
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js', '.json', '.scss'],
+			alias: {
+				api: path.join(__dirname, 'src', 'api'),
+				assets: path.join(__dirname, 'src', 'assets'),
+				config: path.join(__dirname, 'src', 'config'),
+				containers: path.join(__dirname, 'src', 'containers'),
+				components: path.join(__dirname, 'src', 'components'),
+				models: path.join(__dirname, 'src', 'models'),
+				services: path.join(__dirname, 'src', 'services'),
+				style: path.join(__dirname, 'src', 'style')
+			}
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|tsx|js)$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'ts-loader'
+						}
+					]
+				},
+				{
+					enforce: 'pre',
+					test: /\.js$/,
+					loader: 'source-map-loader',
+					exclude: [
+						// instead of /\/node_modules\//
+						path.join(process.cwd(), 'node_modules')
+					]
+				},
+				{
+					test: /\.(jpe?g|png|gif|svg)$/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: 'assets/[hash].[ext]'
+							}
+						}
+					]
+				}
+			].concat(
+				isProd
+					? []
+					: //Run tslint through code in dev environment
+					  [
+							{
+								test: /\.(ts|tsx)/,
+								exclude: /node_modules/,
+								use: [{ loader: 'tslint-loader' }]
+							}
+					  ]
+			)
+		},
+		plugins: [
+			new HtmlWebpackPlugin(
+				Object.assign(
+					{
+						filename: 'index.html',
+						template: path.join(__dirname, 'src', 'index.html'),
+						minify: isProd
+							? {
+									collapseInlineTagWhitespace: true,
+									collapseWhitespace: true,
+									sortAttributes: true,
+									sortClassName: true
+							  }
+							: false
+					},
+					isProd ? { filename: path.join(__dirname, 'dist', 'index.html') } : null
+				)
+			),
+			new FaviconsWebpackPlugin('./src/assets/favicon.png')
+		].concat(
+			//Enable HMR in dev environment
+			isProd ? [] : [new webpack.HotModuleReplacementPlugin()]
+		),
+		devServer: {
+			contentBase: path.join(__dirname, 'dist'),
+			compress: true,
+			publicPath: '/',
+			hot: true,
+			port: 8000,
+			stats: {
+				color: true
+			}
+		}
+	};
+};
