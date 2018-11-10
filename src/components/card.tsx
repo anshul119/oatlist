01fe4a1b2dfac1603df79b124f8de29c5e7d45f1
anshul119 <assharmaanshul119@gmail.com>
2018-11-10 19:47:36 +0100

    added avatars and load more

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Styled from 'styled-components';
+import * as GeoPattern from 'geopattern';
 
 interface ICardProps {
 	name: string;
@@ -7,21 +8,33 @@ interface ICardProps {
 
 const StyledCard = Styled.div`
 	border: 2px solid black;
-	flex-basis: 23%;
+	flex-basis: 22%;
 	margin: 5px;
+	padding: 2px 5px;
 	text-align: center;
+	display: flex;
+	align-items: center;
 
 	p {
 		text-transform: capitalize
 	}
 `;
 
-export default class Card extends React.Component<ICardProps, {}> {
-	render() {
-		return (
-			<StyledCard>
-				<p>{this.props.name}</p>
-			</StyledCard>
-		);
-	}
-}
+const Avatar = Styled.img`
+	height: 48px;
+	width: 48px;
+	margin-right: 5px;
+`;
+
+const Card: React.StatelessComponent<ICardProps> = props => {
+	const pattern = GeoPattern.generate(props.name).toDataUri();
+
+	return (
+		<StyledCard>
+			<Avatar src={pattern} />
+			<p>{props.name}</p>
+		</StyledCard>
+	);
+};
+
+export default Card;
--- src/containers/userlist.tsx
+++ src/containers/userlist.tsx
@@ -6,50 +6,91 @@ import Styled from 'styled-components';
 // import variables from 'style/variables';
 
 interface IUserListState {
+	currentOffset: number;
 	userList: IUser[];
+	hasMore: boolean;
 }
 
 const ListWrapper = Styled.div`
 	display: flex;
 	flex-wrap: wrap;
+	justify-content: center;
+`;
+
+const Text = Styled.p`
+	font-size: 24px;
+	text-align: center;
+`;
+
+const Button = Styled.div`
+	border: 2px solid #2c3e50;
+	padding: 5px 25px;
+	font-size: 18px;
+	cursor: pointer;
+	color: white;
+	background: #34495e;
+	max-width: 100px;
+	margin: 20px auto;
+	text-align: center;
 `;
 
 export default class UserList extends React.Component<{}, IUserListState> {
+	limit: number = 20;
+
 	state = {
-		userList: []
+		currentOffset: 0,
+		userList: [],
+		hasMore: true
 	};
+
+	fetchUserList = (limit?: number, offset?: number) => {
+		return api.user.fetchUsers({ limit, offset }).then((response: IUserProps[]) => {
+			const newUserList = response.map((userProps: IUserProps) => {
+				return new User(userProps);
+			});
+			if (newUserList.length) {
+				console.log('has len', newUserList.length);
+				this.setState(prevState => ({
+					currentOffset: prevState.userList.length + this.limit,
+					userList: prevState.userList.concat(newUserList)
+				}));
+			} else {
+				console.log('no len', newUserList);
+				this.setState({ hasMore: false });
+			}
+		});
+	};
+
 	renderUserList = () => {
 		return this.state.userList.map((user: IUser) => {
 			return <Card key={user.id} name={user.fullName} />;
 		});
 	};
 
-	fetchUserList = (limit?: number, offset?: number) => {
-		return api.user.fetchUsers({ limit, offset }).then((response: IUserProps[]) => {
-			const userList = response.map((userProps: IUserProps) => {
-				return new User(userProps);
-			});
-			this.setState({ userList });
-		});
+	loadUsers = () => {
+		this.fetchUserList(this.limit, this.state.currentOffset);
+	};
+
+	renderLoadMoreButton = () => {
+		console.log(this.state.hasMore);
+		return this.state.hasMore ? (
+			<Button onClick={this.loadUsers}>Load More</Button>
+		) : (
+			<Text>Thats all for now Folks.</Text>
+		);
 	};
 
 	componentDidMount() {
-		window.addEventListener('scroll', this.handleScroll);
-		this.fetchUserList();
-	}
-
-	componentWillUnmount() {
-		window.removeEventListener('scroll', this.handleScroll);
-	}
-
-	handleScroll() {
-		if (window.innerHeight + document.documentElement!.scrollTop === document.documentElement!.offsetHeight) {
-			console.log('fefrg');
-			this.fetchUserList();
-		}
+		this.loadUsers();
 	}
 
 	render() {
-		return <ListWrapper>{this.state.userList.length ? this.renderUserList() : <p>Loading...</p>}</ListWrapper>;
+		const isLoading = !this.state.userList.length;
+		return (
+			<React.Fragment>
+				<ListWrapper>{isLoading ? <Text>Loading...</Text> : this.renderUserList()}</ListWrapper>
+				{isLoading ? null : this.renderLoadMoreButton()}
+			</React.Fragment>
+		);
 	}
 }
